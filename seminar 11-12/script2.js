5f98692c9f8c0fb45a0bb1eec611112d7d0a2ee5
Korneev Yaroslav <90088335+SWEETLENIN@users.noreply.github.com>
2021-12-10 23:01:13 +0300

    7-15 добавлены

--- seminar 11-12/script2.js
+++ seminar 11-12/script2.js
@@ -70,6 +70,257 @@ obj.Method3= function (){console.log("Строка 3")}
 obj.Method1().Method2().Method3()
 
 
+//Задание 7
+let d1 = ['45', '78', '10', '3']
+
+d1[7] = '100';
+console.log(d1)
+
+//Задание 8
+var d2 = [45, 78, 10, 3];
+sum2 = 0;
+for (let i = 0; i < d2.length; i++){
+    sum2 += d2[i]
+}
+
+console.log(sum2)
+
+//Задание 9
+var d3 = [45, 78, 10, 3];
+d3[4] = 100;
+sum3 = 0;
+for(let i = 0; i <d3.length; i++){
+    sum3 += d3[i]
+}
+console.log(sum3)
+
+//Задание 10
+var d4 = [45, 78, 10, 3];
+function my(a,b){
+    return a - b;
+}
+d4.sort(my);
+console.log(d4);
+
+//Задание 11
+function array_to_crt(num) {
+    let d5 = [];
+    for(i=0; i<3; i++) {
+        d5[i] = [];
+        for(j=0; j<4; j++) {
+            d5[i][j] = num;
+        }
+    }
+}
+array_to_crt(5)
+
+
+//Задание 12
+class Vector {
+    constructor(x, y) {
+      this.x = x
+      this.y = y
+    }
+
+    plus(point) {
+      return new Vector(this.x + point.x, this.y + point.y)
+    }
+
+    minus(point) {
+      return new Vector(this.x - point.x, this.y - point.y)
+    }
+
+    get length() {
+      return Math.sqrt(this.x * this.x + this.y * this.y)
+    }
+}
+
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+console.log(new Vector(3, 4).length);
+
+//Задание 13
+function rowHeights(rows) {
+    return rows.map(function(row) {
+        return row.reduce(function(max, cell) {
+            return Math.max(max, cell.minHeight());
+        }, 0);
+    });
+}
+
+function colWidths(rows) {
+    return rows[0].map(function(_, i) {
+        return rows.reduce(function(max, row) {
+            return Math.max(max, row[i].minWidth());
+        }, 0);
+    });
+}
+/*вывод таблицы*/
+function drawTable(rows) {
+    var heights = rowHeights(rows);
+    var widths = colWidths(rows);
+
+    function drawLine(blocks, lineNo) {
+        return blocks.map(function(block) {
+            return block[lineNo];
+        }).join(" ");
+    }
+
+    function drawRow(row, rowNum) {
+        var blocks = row.map(function(cell, colNum) {
+            return cell.draw(widths[colNum], heights[rowNum]);
+        });
+        return blocks[0].map(function(_, lineNo) {
+            return drawLine(blocks, lineNo);
+        }).join("\n");
+    }
+
+    return rows.map(drawRow).join("\n");
+}
+
+function repeat(string, times) {
+    var result = "";
+    for (var i = 0; i < times; i++)
+        result += string;
+    return result;
+}
+
+function TextCell(text) {
+    this.text = text.split("\n");
+}
+TextCell.prototype.minWidth = function() {
+    return this.text.reduce(function(width, line) {
+        return Math.max(width, line.length);
+    }, 0);
+};
+TextCell.prototype.minHeight = function() {
+    return this.text.length;
+};
+TextCell.prototype.draw = function(width, height) {
+    var result = [];
+    for (var i = 0; i < height; i++) {
+        var line = this.text[i] || "";
+        result.push(line + repeat(" ", width - line.length));
+    }
+    return result;
+};
+
+function UnderlinedCell(inner) {
+    this.inner = inner;
+}
+UnderlinedCell.prototype.minWidth = function() {
+    return this.inner.minWidth();
+};
+UnderlinedCell.prototype.minHeight = function() {
+    return this.inner.minHeight() + 1;
+};
+UnderlinedCell.prototype.draw = function(width, height) {
+    return this.inner.draw(width, height - 1)
+        .concat([repeat("-", width)]);
+};
+
+function RTextCell(text) {
+    TextCell.call(this, text);
+}
+RTextCell.prototype = Object.create(TextCell.prototype);
+RTextCell.prototype.draw = function(width, height) {
+    var result = [];
+    for (var i = 0; i < height; i++) {
+        var line = this.text[i] || "";
+        result.push(repeat(" ", width - line.length) + line);
+    }
+    return result;
+};
+
+function dataTable(data) {
+    var keys = Object.keys(data[0]);
+    var headers = keys.map(function(name) {
+        return new UnderlinedCell(new TextCell(name));
+    });
+    var body = data.map(function(row) {
+        return keys.map(function(name) {
+            var value = row[name];
+            // This was changed:
+            if (typeof value == "number")
+                return new RTextCell(String(value));
+            else
+                return new TextCell(String(value));
+        });
+    });
+    return [headers].concat(body);
+}
+function StretchCell(inner, width, height) {
+    this.inner = inner;
+    this.width = width;
+    this.height = height;
+}
+
+StretchCell.prototype.minWidth = function() {
+    return Math.max(this.width, this.inner.minWidth());
+};
+StretchCell.prototype.minHeight = function() {
+    return Math.max(this.height, this.inner.minHeight());
+};
+StretchCell.prototype.draw = function(width, height) {
+    return this.inner.draw(width, height);
+};
+
+var sc = new StretchCell(new TextCell("abc"), 1, 2);
+console.log('Задание 13')
+console.log(sc.minWidth());
+console.log(sc.minHeight());
+console.log(sc.draw(3, 2));
+
+//Задание 14
+function ArraySeq(data) {
+    this.position = -1;
+    this.data = data;
+}
+
+function logFive(seq) {
+    for (var i = 0; i < 5; i++) {
+      if (!seq.next()) {
+        break;
+      }
+      console.log(seq.curr()); 
+    }
+}
+
+ArraySeq.prototype.next = function () {
+    if (this.position >= this.data.length - 1) {
+      return false;
+    }
+    this.position++;
+    return true;
+}
+
+ArraySeq.prototype.curr = function () {
+    return this.data[this.position];
+} 
+
+function RangeSeq(from, to) {
+    this.position = from - 1;
+    this.to = to;
+}
+
+RangeSeq.prototype.next = function () {
+    if (this.position >= this.to) {
+      return false;
+    }
+    this.position++;
+    return true;
+}
+
+RangeSeq.prototype.curr = function () {
+    return this.position;
+} 
+console.log('Задание 14')
+logFive(new ArraySeq([1, 2])); 
+logFive(new RangeSeq(100, 1000)); 
+
+
+
 //задание 15
 var Card = {
     from:"Moscow",
